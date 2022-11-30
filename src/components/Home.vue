c9bd342bc6d47faa7ae1dd70f914ecdb0f58d705
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2022-11-29 19:14:20 -0800

    addedtutorial follow along .vue file

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Hello from Home component</h1>
+    <h1>{{ data }}</h1>
+    <h1>{{ msg }}</h1>
+    <button v-on:click="hello">Call Hello Function</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeVue",
+  props: {
+    data: String,
+    msg: String,
+  },
+  data() {
+    return {
+      show: true,
+    };
+  },
+  // methods: {
+  //   hello(item) {
+  //     alert("hello function called", item);
+  //   },
+  // },
+};
+</script>
