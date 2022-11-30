f47bc6dbb0025822ce6e7ad12ce8ca19511452b3
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2022-11-30 15:21:01 -0800

    composing with component, adding 2 new .vue files

---
+++ src/components/Products.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="child">
+    <h1>Product Component</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductVue",
+};
+</script>
+
+<style scoped>
+.child {
+  background-color: #85c7b1;
+  height: 100px;
+  width: 197px;
+  padding: 30px;
+  margin: 20px;
+}
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="child">
+    <h1>Users Component</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UsersVue",
+};
+</script>
+
+<style scoped>
+.child {
+  background-color: #85c7b1;
+  height: 100px;
+  width: 197px;
+  padding: 30px;
+  margin: 20px;
+}
+</style>
