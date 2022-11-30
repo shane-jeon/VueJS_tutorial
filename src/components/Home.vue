fd7812594454a8c7357e39c5f227425039551ec1
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2022-11-29 19:23:47 -0800

    added code for conditionals

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    <h1>Hello from Home component</h1>
-    <h1>{{ data }}</h1>
+    <!-- conditionals -->
+    <h1 v-if="show">Conditional Rendering in Vue js</h1>
+    <h1 v-else>else side</h1>
+    <button v-on:click="showFunction">toggle</button>
+    <!-- <h1>Hello from Home component</h1> -->
+    <!-- creating components -->
+    <!-- <h1>{{ data }}</h1>
     <h1>{{ msg }}</h1>
-    <button v-on:click="hello">Call Hello Function</button>
+    <button v-on:click="hello">Call Hello Function</button> -->
   </div>
 </template>
 <script>
@@ -15,8 +20,14 @@ export default {
   },
   data() {
     return {
-      show: true,
+      show: false,
     };
+  },
+  // conditional
+  methods: {
+    showFunction() {
+      this.show = !this.show;
+    },
   },
   // methods: {
   //   hello(item) {
