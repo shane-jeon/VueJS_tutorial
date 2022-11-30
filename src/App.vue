68106da4b8259a7ca6c7f64df527e7d72c380b1b
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2022-11-29 19:12:39 -0800

    initial commit, learned how to make component and how to use click event

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Home data="Shane is here" msg="we are friends" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./components/Home.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    Home,
+  },
+};
 </script>
 
 <style>
