cc902b76c9a2eaa019e9e6010889a2c55c436c9e
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2022-11-30 16:37:26 -0800

    props child to parent component

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
     <!-- <Home data="Shane is here" msg="we are friends" /> -->
     <!-- <Home /> -->
     <!-- <DataBind /> -->
-    <Child v-bind:users="users" />
+    <!-- <Child v-bind:users="users" /> -->
+    <Child2 v-on:changeTitle="updateTitleText($event)" />
+    <h1>{{ title }}</h1>
   </div>
 </template>
 
@@ -14,24 +16,36 @@
 // import Home from "./components/Home.vue";
 // import Home from "./components/Home";
 // import DataBind from "./components/DataBind";
-import Child from "./components/Child";
+// import Child from "./components/Child";
+import Child2 from "./components/Child2";
 export default {
   name: "App",
   components: {
     // Home,
     // DataBind,
-    Child,
+    // Child,
+    Child2,
   },
   data() {
     return {
-      users: [
-        { name: "shane", email: "shane@test.com" },
-        { name: "anne", email: "anne@test.com" },
-        { name: "tom", email: "tom@test.com" },
-        { name: "lily", email: "lily@test.com" },
-      ],
+      title: "Child to Parent Data",
     };
   },
+  methods: {
+    updateTitleText(title) {
+      this.title = title;
+    },
+  },
+  // data() {
+  //   return {
+  //     users: [
+  //       { name: "shane", email: "shane@test.com" },
+  //       { name: "anne", email: "anne@test.com" },
+  //       { name: "tom", email: "tom@test.com" },
+  //       { name: "lily", email: "lily@test.com" },
+  //     ],
+  //   };
+  // },
   // components: {
   //   // HelloWorld,
   //   Home,
---
+++ src/components/Child2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button v-on:click="updateTitle">Update Title</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChildComponent",
+  methods: {
+    updateTitle() {
+      // console.warn("updateTitle");
+      // emit takes 2 parameters: 1. event name, 2. Your chosen title
+      this.$emit("changeTitle", "Props have been passed");
+    },
+  },
+};
+</script>
