c7e41f297d686852c1e486b52c3a0d5969f30110
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2022-11-30 16:02:17 -0800

    data binding pt.1

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <!-- <Home data="Shane is here" msg="we are friends" /> -->
-    <Home />
+    <!-- <Home /> -->
+    <DataBind />
   </div>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
 // import Home from "./components/Home.vue";
-import Home from "./components/Home";
+// import Home from "./components/Home";
+import DataBind from "./components/DataBind";
 export default {
   name: "App",
   components: {
-    Home,
+    // Home,
+    DataBind,
   },
   // components: {
   //   // HelloWorld,
